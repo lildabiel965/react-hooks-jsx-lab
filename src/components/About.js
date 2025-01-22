97a82aa06e771ccf0bd17c0838c855511c679d53
Daniel <daniel.mutembei@student.moringaschool.com>
2025-01-22 11:50:38 +0300

    done

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,14 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (<div id="about">
+    <h2>About Me</h2>
+    <p>
+    I am a passionate web developer with a love for creating dynamic and responsive web applications. 
+    I enjoy learning new technologies and improving my skills.
+    </p>
+    <img> sre={image} alt="l made this"</img>
+  </div>);
 }
 
 export default About;
